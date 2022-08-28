e1af6cc21ae392eae0693423cfc9f32899e329bf
Hady Abdelhakem <hadyabdelhakem@gmail.com>
2022-08-28 22:17:42 +0200

    V2/29-08-2022

--- app/src/main/java/com/udacity/asteroidradar/main/ListAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/ListAdapter.kt
@@ -1,22 +1,29 @@
 package com.udacity.asteroidradar.main
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.Asteroid
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.udacity.asteroidradar.R
 
 class ListAdapter(private val onClickListener: OnClickListener): RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     private var asteroidList = emptyList<Asteroid>()
+    private var context: Context? = null
 
     class MyViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
         val textViewId: TextView = itemView.findViewById(R.id.item_id)
+        val textViewDate: TextView = itemView.findViewById(R.id.date_text)
+        val imageView: ImageView = itemView.findViewById(R.id.imageViewStatus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        context = parent.context
         return MyViewHolder(LayoutInflater.from(parent.context)
             .inflate(R.layout.asteroid_item , parent , false))
     }
@@ -24,6 +31,10 @@ class ListAdapter(private val onClickListener: OnClickListener): RecyclerView.Ad
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = asteroidList[position]
         holder.textViewId.text = currentItem.codename
+        holder.textViewDate.text = "("+currentItem.closeApproachDate+")"
+        if (currentItem.isHazadous){
+            holder.imageView.setImageDrawable(context?.let { ContextCompat.getDrawable(it, R.drawable.ic_status_potentially_hazardous) })
+        }
         holder.itemView.setOnClickListener {
             onClickListener.onClick(currentItem)
         }
